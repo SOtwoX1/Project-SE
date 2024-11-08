067ea0ae6f12bb8eff82b543a5e5fa1ef26d0bac
SOtwoX1 <siratee6775@gmail.com>
2024-11-09 00:17:46 +0700

    Welcome_page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
-import Image from "./components/image";
+import Learn from "./components/have_to_see_for_begin/learn";
+import Image from "./components/have_to_see_for_begin/learn2";
+import Welcome from "./components/welcome_page";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/welcome" element={<Welcome />} /> 
+        <Route path="/learn" element={<Learn />} />
         {/* //http://localhost:3000/ */}
         <Route path="/nextpage" element={<Image />} /> 
         {/* //http://localhost:3000/nextpage
--- src/server/main.js
+++ src/server/main.js
@@ -95,3 +95,4 @@ app.get("/hello", (req, res) => {
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
+// http://localhost:3000
