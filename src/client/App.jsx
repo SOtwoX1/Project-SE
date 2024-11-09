b4beeff18a140cedd300eb001d13381c93f5254e
Chanawit Jindatanasit <gget.2547@gmail.com>
2024-11-09 12:35:31 +0700

    fix_home_login_regis_new

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,19 +4,16 @@ import Login from "./components/login";
 import Register from "./components/regis";
 import Learn from "./components/have_to_see_for_begin/learn";
 import Image from "./components/have_to_see_for_begin/learn2";
-import Welcome from "./components/welcome_page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/welcome" element={<Welcome />} /> 
         <Route path="/learn" element={<Learn />} />
         <Route path="/nextpage" element={<Image />} /> 
         <Route path="/home-login-register" element={<HomeLoginAndRegister />} /> 
         <Route path="/Login" element={<Login />} /> 
         <Route path="/Regis" element={<Register />} /> 
-        
       </Routes>
     </BrowserRouter>
   );
