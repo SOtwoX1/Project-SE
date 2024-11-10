4041ae68d1178d2621cf616881f9115923749ca2
Chanawit Jindatanasit <gget.2547@gmail.com>
2024-11-10 23:24:51 +0700

    fix regis and login 2

--- src/client/components/login.jsx
+++ src/client/components/login.jsx
@@ -14,39 +14,42 @@ export default function Login() {
         window.location.href = "http://localhost:3000/Forget-password";
       }
     return (
-        <div className="fixed overflow-hidden w-full h-full flex flex-col space-y-12  mt-[44px] px-[21px]">
-            
-            <Button onClick={go_to_homelog} className="w-[41px] h-[41px] border border-[#E8ECF4] rounded-lg">
-                <svg class="w-[19px] h-[19px] text-gray-800 dark:text-white m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m15 19-7-7 7-7"/>
-                </svg>
-
-            </Button>
-            
-            <form className="flex flex-col justify-center space-y-5">
-                <div className="text-3xl font-semibold w-[280px] h-[78px]">Welcome back! Glad to see you, Again!</div>
+        <div className="fixed overflow-hidden flex flex-col space-y-[285px]">
+                <div className="w-full h-full flex flex-col space-y-[28px]  mt-[56px] px-[21px]">
                 
-                <div className="flex flex-col space-y-3 ">
-                    <input type="email" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enetr your email"/>
-                    <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enter your password "/>
+                    <Button onClick={go_to_homelog} className="w-[41px] h-[41px] border border-[#E8ECF4] rounded-lg">
+                        <svg class="w-[19px] h-[19px] text-gray-800 dark:text-white m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m15 19-7-7 7-7"/>
+                        </svg>
+
+                    </Button>
                     
-                    <Button onClick={go_to_forget} className="text-[#6A707C] ml-auto">Forget Password ?</Button>   
+                    <form className="flex flex-col justify-center space-y-5">
+                        <div className="text-3xl font-semibold w-[280px] h-[78px]">Welcome back! Glad to see you, Again!</div>
+                        
+                        <div className="flex flex-col space-y-3 ">
+                            <input type="email" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enetr your email"/>
+                            <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enter your password "/>
+                            
+                            <Button onClick={go_to_forget} className="text-[#6A707C] ml-auto">Forget Password ?</Button>   
+                        </div>
+
+                    
+                        <Button 
+                        onSubmit={cheackid}
+                        className="w-[331px] h-[56px] bg-[#E9C46A] rounded-lg">
+                            Log in
+                        </Button>    
+                    </form>
+
                 </div>
 
-               
-                <Button 
-                onSubmit={cheackid}
-                className="w-[331px] h-[56px] bg-[#E9C46A] rounded-lg">
-                    Log in
-                </Button>    
-            </form>
-
-            <div className=" mt-auto mx-auto border border-red">
-                <div>
-                    Don't have an account? <Button onClick={go_to_regis} className="text-[#1b998b]">Register Now</Button> 
-                </div> 
+            <div className="text-center">
+                        Don't have an account? <Button onClick={go_to_regis} className="text-[#1b998b]">Register Now</Button> 
             </div>
+
         </div>
+        
         
         
     );
--- src/client/components/regis.jsx
+++ src/client/components/regis.jsx
@@ -11,38 +11,38 @@ export default function Register() {
         const id = {}
       } 
     return(
-        <div className="fixed overflow-hidden w-full h-full flex flex-col space-y-11  mt-[44px] px-[21px]">
-            
-            <Button onClick={go_to_homelog} className="w-[41px] h-[41px] border border-[#E8ECF4] rounded-lg">
-                <svg class="w-[19px] h-[19px] text-gray-800 dark:text-white m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m15 19-7-7 7-7"/>
-                </svg>
-
-            </Button>
-
-            <form className="flex flex-col justify-center space-y-5">
-                <div className="text-3xl font-semibold w-[331px] h-[78px]">Hello! Register to get started</div>
+        <div className="fixed overflow-hidden flex flex-col space-y-[184px]">
+            <div className="w-full h-full flex flex-col space-y-[28px]  mt-[56px] px-[21px]">
                 
-                <div className="flex flex-col space-y-3 ">
-                    <input type="text" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Uesrname"/>
-                    <input type="Email" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Email"/>
-                    <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enter your password "/>
-                    <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Confirm password "/>   
-                </div>
+                <Button onClick={go_to_homelog} className="w-[41px] h-[41px] border border-[#E8ECF4] rounded-lg">
+                    <svg class="w-[19px] h-[19px] text-gray-800 dark:text-white m-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m15 19-7-7 7-7"/>
+                    </svg>
+                </Button>
 
-               
-                <Button 
-                onSubmit={check_regis}
-                className="w-[331px] h-[56px] bg-[#E9C46A] rounded-lg">
-                    Register
-                </Button>    
-            </form>
-
-            <div className=" mt-auto mx-auto border border-red">
-                <div>
-                Already have an account?<Button onClick={go_to_login} className="text-[#1b998b]">Login Now</Button> 
-                </div> 
+                
+                <form className="flex flex-col space-y-5 space">
+                        <div className="text-3xl font-semibold w-[331px] h-[78px]">Hello! Register to get started</div>
+                        
+                        <div className="flex flex-col space-y-3 ">
+                            <input type="text" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Uesrname"/>
+                            <input type="Email" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Email"/>
+                            <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Enter your password "/>
+                            <input type="password" className="w-[331px] h-[56px] bg-[#F7F8F9] placeholder-[#8391A1] border border-[#E8ECF4] rounded-lg" placeholder="Confirm password "/>   
+                        </div>
+                    <Button
+                        onSubmit={check_regis}
+                        className="w-[331px] h-[56px] bg-[#E9C46A] rounded-lg">
+                            Register
+                    </Button>
+                            
+                </form>
             </div>
+
+            <div className="text-center">                        
+                Already have an account?<Button onClick={go_to_login} className="text-[#1b998b]">Login Now</Button>
+            </div>
+
         </div>
     );
 }
