cda1f0afae445663592ee66ed416d74dad01720e
Chanawit Jindatanasit <gget.2547@gmail.com>
2024-11-18 17:28:25 +0700

    setting page

--- src/client/components/setting_profile.jsx
+++ src/client/components/setting_profile.jsx
@@ -1,6 +1,42 @@
+import Swal from 'sweetalert2';
+
 export default function Setting_pro() {
     const go_to_profile = () => {
         window.location.href = "/profile";
+    }
+    const go_to_showMe = () => {
+        window.location.href = "/profile";
+    }
+    const go_to_Password = () => {
+        window.location.href = "/profile";
+    }
+    const go_to_MPA = () => {
+        window.location.href = "/profile";
+    }
+    const go_to_package = () => {
+        window.location.href = "/profile";
+    }
+    const go_to_login = () => {
+        window.location.href = "/Login";
+    }
+    const Delete_Account = () => {
+        Swal.fire({
+            title: "<u>Delete My Account </u>",
+            text: "All data within the account will be deleted.",
+            showCancelButton: true,
+            confirmButtonColor: "#E76F51",
+            cancelButtonColor: "#F4A261",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText : "Don’t Delete My Account"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your account has been deleted.",
+                icon: "success"
+              });
+            }
+          });
     }
     const email = {
         "email": "@gmail.com",
@@ -8,16 +44,16 @@ export default function Setting_pro() {
     return(
         
         <div 
-          className="w-[375px] h-[717px] rounded-b-[50px] text-[45px] font-extrabold text-[#E76F51] flex flex-col p-3 pt-[8px]" 
+          className="w-[375px] h-[812px] rounded-b-[50px] text-[45px] font-extrabold text-[#E76F51] flex flex-col p-3 pt-[8px] " 
           style={{ fontFamily: 'Abhaya Libre, sans-serif' }}>
-            <div className="flex flex-row items-center justify-center border border-red">
+            <div className="flex flex-row items-center justify-center">
                 <img className="w-[55px] h-[55px] mr-4" src="src/client/img/French Fries.png" alt="French Fries" />
                 <img className="w-[22px] h-[27px]  mt-12 absolute left-[20%]" src="src/client/img/heart.png" alt="Heart" />
                 <span className="text-[#E76F51] text-[45px] font-extrabold">SETTINGS</span>
                 <img className="w-[22px] h-[27px] mt-[-40px] absolute right-[20%]" src="src/client/img/heart2.png" alt="Heart" />
                 <img className="w-[55px] h-[55px] ml-4" src="src/client/img/pizza.png" alt="Pizza" />
             </div>
-            <div className="flex flex-col  divide-y divide-blue border border-red">
+            <div className="flex flex-col  divide-y divide-blue">
                 <div className="flex items-center h-[65px]">
                     <button onClick={go_to_profile} >
                         <svg class="w-[47px] h-[47px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -26,47 +62,74 @@ export default function Setting_pro() {
                     </button>
                     <p className="text-2xl text-black">PROFILE</p>
                 </div>
-                <div className="flex justify-between text-2xl  items-center h-[65px]">
+                <div className="flex justify-between text-xl  items-center h-[65px]">
                     <p className="text-black p-12">Email</p>
                     <p className="text-[#D9D9D9]">{email.email}</p>
                 </div>
-                <div className="flex justify-between text-2xl  items-center h-[65px]">
+                <div className="flex justify-between text-xl  items-center h-[65px]">
                     <p className="text-black p-12">Show Me</p>
-                    <button>
+                    <button onClick={go_to_showMe}>
                         <svg class="w-[29px] h-[29px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m9 5 7 7-7 7"/>
                         </svg>
                     </button>
                 </div>
-                <div className="flex justify-between text-2xl  items-center h-[65px]">
+                <div className="flex justify-between text-xl  items-center h-[65px]">
                     <p className="text-black p-12">Password</p>
-                    <button>
+                    <button onClick={go_to_Password}>
                         <svg class="w-[29px] h-[29px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m9 5 7 7-7 7"/>
                         </svg>
                     </button>
                 </div>
-                <div className="flex justify-between text-2xl  items-center h-[65px]">
+                <div className="flex justify-between text-xl  items-center h-[65px]">
                     <p className="text-black p-12">Manage Payment Account</p>
-                    <button>
+                    <button onClick={go_to_MPA}>
                         <svg class="w-[29px] h-[29px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m9 5 7 7-7 7"/>
                         </svg>
                     </button>
                 </div>
-                <div className="flex justify-between text-2xl  items-center h-[65px]">
+                <div className="flex justify-between text-xl  items-center h-[65px]">
                     <p className="text-black p-12">My Package</p>
-                    <button>
+                    <button onClick={go_to_package}>
                         <svg class="w-[29px] h-[29px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m9 5 7 7-7 7"/>
                         </svg>
                     </button>
                 </div>
-                <div className="flex justify-between text-2xl h-[50px] items-center ">
+                
+                <div className="text-center pt-[25px]">
+                    <button 
+                    onClick={go_to_login}
+                    className="w-[331px] h-[39px] rounded-lg bg-red text-xl text-white">
+                        Logout
+                    </button>
                 </div>
-                <button className="w-[331px] h-[39px] rounded-lg bg-red text-xl text-white">
-                    Logout
-                </button>
+                
+            </div>
+
+            <div className="flex flex-col space-y-[15px] mt-[15px]">
+                <div className="text-center text-black text-3xl">
+                    <p>Contact Us</p>
+                </div>
+
+                <div className="flex flex-row items-center gap-[25px] w-[266px] h-[77px] m-auto">
+                    <img className="w-[77px] h-[77px]" src="src\client\img\LINE.png" alt="Line" />
+                    <img className="w-[62px] h-[62px]" src="src\client\img\Facebook.png" alt="Facebook" />
+                    <img className="w-[77px] h-[77px]" src="src\client\img\TwitterX.png" alt="X" />
+                </div>
+
+                <div className="text-center">
+                    <button 
+                    onClick={Delete_Account}
+                    className="w-[331px] h-[39px] rounded-lg bg-red text-xl text-white">
+                        Delete Account
+                    </button>
+                </div>
+                
+                
+
             </div>
             
         </div>
