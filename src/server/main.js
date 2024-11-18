e5abb2750842ee66c5ef0644243628bd815a33c6
Kamolluk Seaaung <pairkamolluk@gmail.com>
2024-11-19 01:56:48 +0700

    113

--- src/server/main.js
+++ src/server/main.js
@@ -39,7 +39,8 @@ app.get('/api/protected-route', authenticateToken, (req, res) => {
 //-----------------------------------------------------------------------------------------------
 
 // MongoDB Connection
-const mongoURI = 'mongodb+srv://Otwo:1234@se-project.qqqt0.mongodb.net/DatingApp?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://pair:1234@se-project.qqqt0.mongodb.net/DatingApp?retryWrites=true&w=majority';
+
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully to DatingApp!'))
